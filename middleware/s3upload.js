64c66e4570230a655f11930d19f10cbce0c803a8
adharsh-A <103622525+adharsh-A@users.noreply.github.com>
2024-09-05 15:26:04 +0530

    after s3

---
+++ middleware/s3upload.js
@@ -0,0 +1,42 @@
+import multer from 'multer';
+import AWS from 'aws-sdk';
+import { v1 as uuid } from "uuid";
+
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+});
+
+// Set up multer to store files in memory
+const upload = multer({ storage: multer.memoryStorage() });
+
+// Middleware function for uploading to S3
+export const uploadFileToS3 = (req, res, next) => {
+  // First, run multer to parse the file
+  upload.single('image')(req, res, (err) => {
+    if (err) {
+      return res.status(500).json({ error: 'File upload failed' });
+    }
+    const uniqueFileName = `${uuid()}-${req.file.originalname}`;
+
+    // Define S3 upload parameters
+    const params = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: uniqueFileName, // File name to save in S3
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype,
+    };
+
+    // Upload the file to S3
+    s3.upload(params, (error, data) => {
+      if (error) {
+        return res.status(500).json({ error });
+      }
+      
+      // Save the S3 response data to request for later use
+      req.s3Data = data;
+      next(); // Proceed to the next middleware
+    });
+  });
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,7 @@ import express from 'express';
 const router = express.Router();
 import {getAllProducts,getProductsById,addProduct} from "../controllers/product-controllers.js";
 import fileUpload from "../middleware/fileupload.js";
+import { uploadFileToS3 } from '../middleware/s3upload.js';
 import { authorizeRoles, isAuthenticatedUser } from '../middleware/auth.js';
 
 router.get(
@@ -10,6 +11,8 @@ router.get(
   getAllProducts
 );
 router.get('/product/:id', getProductsById);
+
+router.post("/admin/add",uploadFileToS3,addProduct);
 router.post("/admin/add",fileUpload.single('image'),addProduct);
 
 export default router;
