3c08a97149f52e9e3a3005372b541cb83439cdfa
adharsh-A <103622525+adharsh-A@users.noreply.github.com>
2024-09-05 12:56:43 +0530

    origin

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 // CORS configuration
 const corsOptions = {
-  origin: "*", // Allows all origins
+  origin: ['https://jumpsquad.vercel.app'], // Allows all origins
   methods: "GET, POST, PATCH, DELETE, PUT,OPTIONS",
   allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
 };
