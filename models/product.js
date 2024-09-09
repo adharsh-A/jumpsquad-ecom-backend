cd7255cef8498ced96f64556cca8c34e83178556
adharsh-A <103622525+adharsh-A@users.noreply.github.com>
2024-09-09 18:02:54 +0530

    after checking schema

--- models/product.js
+++ models/product.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  productId: {type:String,required:true,trim:true},
     title: {type:String,required:true,trim:true},
     price: { type: Number, required: true },
     description: { type: String, required: true },
